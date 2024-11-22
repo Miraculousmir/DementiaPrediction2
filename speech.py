84d86c2c0a618fa2d12e5daee908aefc166b2eb7
Raunak Tiwari <99192408+19Raunak@users.noreply.github.com>
2024-11-22 23:33:03 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,4 +1,4 @@
-from spellchecker import SpellChecker
+# from spellchecker import SpellChecker
 import pickle
 import streamlit as st
 import speech_recognition as sr
@@ -11,7 +11,7 @@ import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-spell = SpellChecker()
+# spell = SpellChecker()
 # Tokenize function (same as before)
 def tokenize(speech_text):
     for match in re.finditer(r'\w+', speech_text, re.UNICODE):
@@ -163,12 +163,12 @@ def show_page():
         # Compute cosine similarity between the two vectors
         similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
         #rounded_similarity= round(similarity[0][0],5)
-        taken_prob= cosine_to_probability_piecewise(similarity[0][0])
-        words = text.split()
+        taken_prob= similarity[0][0]
+        # words = text.split()
         misspelled = spell.unknown(words)
-        if similarity == 0.0 and len(misspelled) == 0:
-            st.write("You are talking out of context. Please try again. ")
-        else:
-            st.write(f"Probability of having Dementia out of 10: {dementia_prob_rounded[0]}")
+        # if similarity == 0.0 and len(misspelled) == 0:
+        # st.write("You are talking out of context. Please try again. ")
+        # else:
+        st.write(f"Probability of having Dementia out of 10: {int(dementia_prob_rounded[0] * (10 - (taken_prob* 10)))}")
         # Display the prediction result
         ###st.write(f"{rounded_similarity}")
